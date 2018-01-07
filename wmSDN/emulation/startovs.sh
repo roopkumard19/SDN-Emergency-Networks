73271fefd8d98e4c0f78d892486122f29b0a9dcc
roopkumard19 <roopkumar_d19@hotmail.com>
2018-01-06 21:43:45 -0800

    updated startovs.sh

--- wmSDN/emulation/startovs.sh
+++ wmSDN/emulation/startovs.sh
@@ -3,15 +3,19 @@
 SERVICENAME="openvswitchservice_start.sh"
 
 corevcmd() {
-	node=$1
-	shift
-	vcmd  -c /tmp/$(ls -lt /tmp/ | grep pycore | head -n 1 | awk '{print $NF}')/$node -- $@
+        node=$1
+        shift
+        vcmd  -c /tmp/$(ls -lt /tmp/ | grep pycore | head -n 1 | awk '{print $NF}')/$node -- $@
 }
 
 for node in /tmp/$(ls -lt /tmp/ | grep pycore | head -n 1 | awk '{print $NF}')/*.pid; do
-	nodename=$(basename $node .pid);
-	corevcmd $nodename bash $SERVICENAME start
-	corevcmd $nodename bash ./o2o.sh > /tmp/${nodename}_o2o.log 2>&1 &
-	corevcmd $nodename bash ./emergency_flows.sh > /tmp/${nodename}_emer_flows.log 2>&1 &
+        nodename=$(basename $node .pid);
+        if [ "$nodename" != "controller" -a "$nodename" != "server" -a "$nodename" != "client" ]
+        then
+#       echo $nodename
+        corevcmd $nodename bash $SERVICENAME start
+        corevcmd $nodename bash ./o2o.sh > /tmp/${nodename}_o2o.log 2>&1 &
+        corevcmd $nodename bash ./emergency_flows.sh > /tmp/${nodename}_emer_flows.log 2>&1 &
+        fi
 done
 
