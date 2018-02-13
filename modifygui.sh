e28b7542f4a717e290f6dd54c5322aa886e84731
Abhilash Tuse <abhilashtuse@gmail.com>
2018-02-13 15:52:14 -0800

    fix uav configuration and add them to core canvas

--- modifygui.sh
+++ modifygui.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 
-# Move these 4 lines in python script, And attach drones to wlan1, get ip and mac addresess for them
-coresendmsg node flags=add number=12 type=0 name='n12' xpos=860 ypos=650 icon=/usr/lib/core/icons/normal/drone.gif
-coresendmsg node flags=add number=13 type=0 name='n13' xpos=906 ypos=650
-coresendmsg node flags=add number=14 type=0 name='n14' xpos=842 ypos=600
-coresendmsg node flags=add number=15 type=0 name='n15' xpos=891 ypos=604
+#./modifygui.sh {Failed node number} {Drone_node__start_id} {Drone_node__end_id}
+# example: 
+# ./modifygui.sh 2 10 13
 
-coresendmsg link n1number=3 n2number=9 guiattr='color=black'
-coresendmsg link n1number=1 n2number=8 guiattr='color=black'
-coresendmsg link n1number=6 n2number=$1 guiattr='color=black'
-coresendmsg link n1number=7 n2number=$1 guiattr='color=black'
-coresendmsg link n1number=5 n2number=$1 guiattr='color=black'
+#coresendmsg link n1number=3 n2number=9 guiattr='color=black'
+#coresendmsg link n1number=1 n2number=8 guiattr='color=black'
+#coresendmsg link n1number=6 n2number=$1 guiattr='color=black'
+#coresendmsg link n1number=7 n2number=$1 guiattr='color=black'
+#coresendmsg link n1number=5 n2number=$1 guiattr='color=black'
 
 sleep 10
-coresendmsg node number=$2 icon=/usr/lib/core/icons/normal/router_red.gif
+
+START=$2
+END=$3
+
+
+for (( c=$START; c<=$END; c++ ))
+do
+	coresendmsg node number=$c icon=/usr/lib/core/icons/normal/drone.gif
+done
+
+coresendmsg node number=$1 icon=/usr/lib/core/icons/normal/router_red.gif
 sleep 3
 coresendmsg node number=12 name='n12' xpos=770 ypos=613
 sleep 1
